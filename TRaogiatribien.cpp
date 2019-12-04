5e4e920488844a09ac781d9d00d059adadb412e4
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-04 18:14:34 +0700

    Create TRaogiatribien.cpp

---
+++ TRaogiatribien.cpp
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main ()
+{ int x;
+	printf ("Nhap gia tri cho so x \n");
+	scanf ("%d", & x);
+	int y;
+	printf ("Nhap gia tri cho so y \n");
+	scanf ("%d", &y);
+	x = y *1;
+	y = x * 1;
+	printf ("Doi gia tri 2 bien voi nhau \n");
+	printf ("So X moi la %d \n", x);
+	printf ("So Y moi la %d \n", y);
+	}
