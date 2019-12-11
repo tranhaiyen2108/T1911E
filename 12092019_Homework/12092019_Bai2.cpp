2e04889534d77686a3100389c94418e6e6eee25c
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-11 18:06:42 +0700

    Update 12092019_Bai2.cpp

--- 12092019_Homework/12092019_Bai2.cpp
+++ 12092019_Homework/12092019_Bai2.cpp
@@ -1,27 +1,17 @@
-#inlude <stdio.h>
+#include <stdio.h>
 
 int main ()
 
 {
-
 	int a, b, c;
-
 	printf (" Nhap 3 so a, b, c la 3 canh tam giac \n");
-
 	do {
-
 		printf (" Nhap a \n");
-
 		scanf (%d,& a);
-
 		printf (" Nhap b \n");
-
 		scanf (%d,& b);
-
 		printf (" Nhap c \n");
-
 		scanf (%d,& c);
-
 		printf (" 3 so a, b, c khong hop le. Nhap lai \n");
 
 	} while ((a + b) > c || (c + b) > a || (a + c) > b);
