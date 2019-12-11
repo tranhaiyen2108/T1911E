8f7e3fec8d8d4f3e754c1b5f7c2d32b6fde93f6f
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-11 18:06:45 +0700

    Update 12092019_Bai3.cpp

--- 12092019_Homework/12092019_Bai3.cpp
+++ 12092019_Homework/12092019_Bai3.cpp
@@ -1,38 +1,22 @@
-#inlude <stdio.h>
+#include <stdio.h>
 
 int main ()
 
 {
-
 	int n, count;
-
 	count = 0;
-
-	printf ("Nhap vao so n muon kiem tra co ph?i so nguyen to khong, n = \n");
-
-	scanf ("%d \n",&n);
-
+	printf ("Nhap vao so n muon kiem tra co phai so nguyen to khong, n = \n");
+	scanf ("%d",&n);
 	for (int i = 1; i < n; (n%i) == 0) {
-
 		count = count + 1;
-
 		i++;
-
 	}
-
-	if (count > 2) {
-
+	if (count > 1) {
 		printf ("%d khong phai la so nguyen to\n",n);
-
 	}
-
 	else {
-
 		printf ("%d la so nguyen to\n",n);
-
 	}
-
 	return 0;
-
 }
 
