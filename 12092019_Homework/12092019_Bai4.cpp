606a8b119799aae21312a389a744df3cbbcff5dd
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-11 18:19:16 +0700

    Update 12092019_Bai4.cpp

--- 12092019_Homework/12092019_Bai4.cpp
+++ 12092019_Homework/12092019_Bai4.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
-int main(){
+int main ()
+{
     int n, m;
 	printf ("Nhap vao so n muon doan nguoc \n");
 	printf ("n = ");
     scanf("%d", &n);
-   for (int reverse-n = 0; n >0; n/10){
+   for (int reverse_n = 0; n >0; n/10){
         m = n % 10;
-        reverse-n = reverse-n * 10 + m;
+        reverse_n = reverse_n * 10 + m;
         n = n / 10;
     }
-    printf("%d", reverse-n);
+    printf("%d", reverse_n);
+    return 0;
 }
