cba535eba82246a48471a1a7d32437561ed46800
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-11 17:44:14 +0700

    Create 12092019_Bai1.cpp

---
+++ 12092019_Homework/12092019_Bai1.cpp
@@ -0,0 +1,30 @@
+
+#inlude <stdio.h>
+
+int main ()
+
+{
+
+int n;
+
+printf ("Viet phuong trinh tinh n!\n");
+
+printf ("Nhap vao 1 so nguyen n \n");
+
+scanf (%d, &n);
+
+float s =0;
+
+for (int i = 0; (i+1) <= n; i++) {
+
+		s = i*(i+1);
+
+
+
+	}
+
+printf ("Ket qua phuong trinh n! = %f", s)
+
+	return 0;
+
+}
