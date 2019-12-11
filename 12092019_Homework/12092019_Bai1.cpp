40e5037688e90d2ec4d9b4e1a4a3972886ca2d14
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-11 18:06:35 +0700

    Update 12092019_Bai1.cpp

--- 12092019_Homework/12092019_Bai1.cpp
+++ 12092019_Homework/12092019_Bai1.cpp
@@ -1,30 +1,16 @@
 
-#inlude <stdio.h>
+#include <stdio.h>
 
 int main ()
-
 {
-
 int n;
-
 printf ("Viet phuong trinh tinh n!\n");
-
 printf ("Nhap vao 1 so nguyen n \n");
-
 scanf (%d, &n);
-
 float s =0;
-
 for (int i = 0; (i+1) <= n; i++) {
-
 		s = i*(i+1);
-
-
-
 	}
-
 printf ("Ket qua phuong trinh n! = %f", s)
-
 	return 0;
-
 }
