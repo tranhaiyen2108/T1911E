3b2e2cb7dcb4ff157d3d5a5baa361720ad00b641
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-21 19:01:34 +0700

    Create Bai2.cpp

---
+++ 12182019_session 8/Bai2.cpp
@@ -0,0 +1,34 @@
+//Tim so thu n trong day fibonacci 
+#include <stdio.h>
+
+int timkiem(unsigned int n){
+    int x =0;
+    if (n <1){
+        x = 0;
+        return x;
+    }else if (n <=2){
+            x = 1;
+            return x;
+    }else if (n ==3){
+    	x = 2;
+    	return x;
+	}else if (n>=4 ){
+        int x1=1, x2 = 2;
+        for (int i =4; i<= n; i++){
+			x = x1+x2;
+            x1 = x2;    
+            x2 = x;
+        }
+    } return x;
+        
+}
+
+int main()
+{
+    int n;
+    printf("nhap vao so n = ");
+    scanf ("%d",&n);
+    printf ("So thu %d trong day Fibonacci la: %d",n,timkiem(n));
+    
+    return 0;
+}
