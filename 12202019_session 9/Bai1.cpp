a0294a1f00c038d8fbaf55c2fa433ec5a79127f4
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-22 14:26:05 +0700

    Create Bai1.cpp

---
+++ 12202019_session 9/Bai1.cpp
@@ -0,0 +1,31 @@
+//1. Nhap 1 mang n chuoi tu bàn phím. Sau dó nhap 1 chuoi s và kiem tra xem s có nam trong mang hay không.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n;
+	printf("Nhap vao n = ");
+	scanf("%d",&n);
+    char array[n][50];
+    for(int i=0;i<n;i++)
+    {
+        printf(" Nhap vao ten hoc sinh thu %d: ",i+1);
+        scanf("%s",array[i]);
+    }
+    char s[50],d[50];
+    printf ("Nhap vao mot ten de kiem tra: ");
+    scanf("%s",s);
+	for (int i = 0;i<n;i++){
+		if (strcmp(array[i],s)==0){
+			strcpy(array[i],d);
+		}
+	}
+	if (strcmp(d,s)==0){
+	printf("Ten %s co nam trong chuoi",s);
+	}else {
+	printf("Ten %s khong co nam trong chuoi",s);
+	}
+	
+	return 0;
+}
+
