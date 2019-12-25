0b0e4d804897b2268063fa59d26f9b84b869eebe
tranhaiyen2108 <tranhaiyen2108@yahoo.com>
2019-12-25 19:47:04 +0700

    Update 19110011_tamgiac.cpp

--- 12252019_Thi/19110011_tamgiac.cpp
+++ 12252019_Thi/19110011_tamgiac.cpp
@@ -24,9 +24,8 @@ int main (){
 		scanf("%lf",&b);
 		printf("Nhap vao canh c = ");
 		scanf("%lf",&c);
-	printf("\n");	
 	while (a<=0 ||b<=0 || c<=0 || c>=(a+b) || b>=(a+c) || a>=(b+c)){
-		printf("Ban da nhap sai dieu kien canh tam giac. Nhap lai\n");
+		printf("\n Ban da nhap sai dieu kien canh tam giac. Nhap lai\n");
 		printf("Cac canh phai lon hon 0, mot canh khong lon hon tong hai canh con lai\n\n");
 		printf("Nhap vao canh a = ");
 		scanf("%lf",&a);
@@ -35,9 +34,8 @@ int main (){
 		printf("Nhap vao canh c = ");
 		scanf("%lf",&c);
 	}
-	printf("\n");
-	printf("Chu vi tam giac la: %lf\n",chuvi(a,b,c));
-	printf ("Dien tich cua tam giac la: %lf",dientich(a,b,c));
+	printf("\n Chu vi tam giac la: %lf\n",chuvi(a,b,c));
+	printf ("\n Dien tich cua tam giac la: %lf",dientich(a,b,c));
 	
 	return 0;
 }
